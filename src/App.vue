e69e122bdae7149cdd5c2c681b75b244d6bf7608
Mam Thenebo <mmshooreshi@gmail.com>
2024-01-20 11:03:49 +0330

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <PdfMenu msg="Hoormazd Menu :D" />
+  </div>
+</template>
+
+<script>
+import PdfMenu from './components/PdfMenu.vue'
+
+export default {
+  name: 'App',
+  components: {
+    PdfMenu
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin: -10px;
+}
+</style>
---
+++ src/components/PdfMenu.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <!-- <h1>File</h1> -->
+    <vue-pdf-embed :source="source1" />
+
+    <!-- <h1>Base64</h1>
+    <vue-pdf-embed :source="source2" /> -->
+  </div>
+</template>
+
+<script>
+import VuePdfEmbed from 'vue-pdf-embed'
+
+// OR THE FOLLOWING IMPORT FOR VUE 2
+// import VuePdfEmbed from 'vue-pdf-embed/dist/vue2-pdf-embed'
+
+export default {
+  components: {
+    VuePdfEmbed,
+  },
+  data() {
+    return {
+      source1: '/menus/hoormazd.pdf',
+      source2: 'data:application/pdf;base64,<BASE64_ENCODED_PDF>',
+    }
+  },
+}
+</script>
